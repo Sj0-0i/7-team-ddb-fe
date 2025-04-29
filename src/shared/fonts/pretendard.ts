d8a46dcdea76124ec5a1cd9df264e1fe1723b0cd
SUZY KANG <67568160+S2UZY@users.noreply.github.com>
2025-04-29 22:16:49 +0900

    Style/#12 기본 폰트 설정 (#14)
    * chore: pretendard 폰트 파일 추가
    
    * style: next/font/local 을 이용한 폰트 구성
    
    * style: 레이아웃 및 global.css 폰트 적용

---
+++ src/shared/fonts/pretendard.ts
@@ -0,0 +1,9 @@
+import localFont from 'next/font/local';
+
+export const pretendard = localFont({
+  src: './PretendardVariable.woff2',
+  display: 'swap',
+  weight: '100 900',
+  style: 'normal',
+  variable: '--font-pretendard',
+});
