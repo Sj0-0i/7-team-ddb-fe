b2eaff6b86d946aed4a2da743e6c4a305b8fdce9
SUZY KANG <67568160+S2UZY@users.noreply.github.com>
2025-05-06 21:29:29 +0900

    Feat/#29 다이어리 페이지 구성 (#30)
    feat: 다이어리 페이지 구성 (#30)

---
+++ src/app/(main)/moments/page.tsx
@@ -0,0 +1,12 @@
+import { Header } from '@/shared/components';
+
+export default function Moments() {
+  return (
+    <>
+      <Header title="다이어리" />
+      <div className="flex h-[calc(100vh-64px)] items-center justify-center text-center text-gray-500">
+        곧 여러분을 찾아갈 새로운 기능을 준비 중이에요!
+      </div>
+    </>
+  );
+}
