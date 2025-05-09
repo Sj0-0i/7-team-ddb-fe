2676b5ec9fef0976fa06c86701db0cd0466250d0
suzykang <pc_suzi@daum.net>
2025-05-09 22:40:55 +0900

    fix: ssg로 렌더링 되는 부분, crs 렌더링 되도록 수정 (현재 경로가 로컬로 인식되는 오류)

--- src/app/(auth)/onboarding/page.tsx
+++ src/app/(auth)/onboarding/page.tsx
@@ -1,14 +1,13 @@
+import { Suspense } from 'react';
+
 import {
   DolpinLogo,
   KakaoLoginButton,
   OnboardingSlider,
   slides,
 } from '@/features/onboarding';
-import { getOAuthRedirectUrl } from '@/features/user';
 
 export default async function Onboarding() {
-  const { redirect_url } = await getOAuthRedirectUrl();
-
   return (
     <div className="relative min-h-screen bg-white">
       <div className="absolute top-0 left-2 z-10">
@@ -17,7 +16,9 @@ export default async function Onboarding() {
       <div className="flex min-h-screen flex-col items-center justify-center pt-20">
         <OnboardingSlider slides={slides} />
         <div className="mt-8 w-full max-w-md px-4">
-          <KakaoLoginButton redirectUrl={redirect_url} />
+          <Suspense>
+            <KakaoLoginButton />
+          </Suspense>
         </div>
       </div>
     </div>
--- src/features/onboarding/components/kakao-login-button/KakaoLoginButton.tsx
+++ src/features/onboarding/components/kakao-login-button/KakaoLoginButton.tsx
@@ -2,18 +2,28 @@
 
 import { NavArrowRight } from 'iconoir-react';
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
-export interface KakaoLoginButtonProps {
-  redirectUrl: string;
-}
+import { getOAuthRedirectUrl } from '@/features/user';
 
-export function KakaoLoginButton({ redirectUrl }: KakaoLoginButtonProps) {
+export function KakaoLoginButton() {
+  const [redirectUrl, setRedirectUrl] = useState<string | null>(null);
+
+  useEffect(() => {
+    const handleRedirect = async () => {
+      const { redirect_url } = await getOAuthRedirectUrl();
+      setRedirectUrl(redirect_url);
+    };
+    handleRedirect();
+  }, []);
+
   return (
     <button
       className="button-text flex w-full items-center justify-center gap-2 px-4 py-2"
       onClick={() => {
-        console.log('redirectUrl', redirectUrl);
-        window.location.href = redirectUrl;
+        if (redirectUrl) {
+          window.location.href = redirectUrl;
+        }
       }}
     >
       <div className="flex items-center gap-2">
