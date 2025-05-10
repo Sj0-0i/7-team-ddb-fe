0958ec3f4504f3c4c87db3ae1ae407eff70b5185
suzykang <pc_suzi@daum.net>
2025-05-10 18:05:43 +0900

    chore: 명시적으로 base url 설정

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  env: {
+    NEXT_PUBLIC_API_BASE_URL: 'https://be.dev.dolpin.site',
+  },
   /* config options here */
 };
 
