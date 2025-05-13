313d21733395af0bf530d788e92004602e1c99d9
SUZY KANG <67568160+S2UZY@users.noreply.github.com>
2025-05-13 21:15:53 +0900

    Feat/#53 로그인 미들웨어 추가 (#55)
    * chore: next config 파일 환경 변수 참조하도록 수정
    
    * feat: 로그인 미들웨어 추가
    
    * chore: callback 페이지 불필요한 주석 삭제
    
    * chore: 맵 페이지 불필요한 에러 메시지 삭제

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   env: {
-    NEXT_PUBLIC_API_BASE_URL: 'https://be.dolpin.site',
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+    NEXT_PUBLIC_KAKAO_MAP_API_KEY: process.env.NEXT_PUBLIC_KAKAO_MAP_API_KEY,
   },
   /* config options here */
 };
