67b6cf30a148be4955fdf0f13fab7c13f0551405
Jiyoung Shin <sjy140801@dgu.ac.kr>
2025-06-26 11:29:09 +0900

    test

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -12,7 +12,8 @@ fi
 
 AWS_REGION="ap-northeast-2"
 ACCOUNT_ID=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | grep accountId | awk -F\" '{print $4}')
-REPO_NAME="dolpin-frontend-${DEPLOYMENT_GROUP_NAME##*-}"  # 예: dev 또는 prod
+ENV_NAME=$(echo "$DEPLOYMENT_GROUP_NAME" | cut -d'-' -f2)
+REPO_NAME="dolpin-frontend-${ENV_NAME}"
 IMAGE_TAG=$(cat /home/ubuntu/app/.image_tag)
 IMAGE="${ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPO_NAME}:${IMAGE_TAG}"
 
